26b51e0d96e1e6bc8d1a6a7fc74c18423279cd1b
John Teske <john.teske@protonmail.com>
2020-11-27 17:21:12 -0800

    Add a ghosty friend

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,23 @@ use thruster::{async_middleware, middleware_fn};
 use thruster::{App, BasicContext as Ctx, Request, Server, ThrusterServer};
 use thruster::{MiddlewareNext, MiddlewareResult};
 
+static ICON: &str = "data:image/x-icon;base64,AAABAAEAEBAQAAEABAAoAQAAFgAAACgAAAAQAAAAIAAAAAEABAAAAAAAgAAAAAAAAAAAAAAAEAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAD//wAAznMAANWrAADb2wAA33sAAN47AADQCwAA3/sAANWrAADb2wAA1asAAN/7AADv9wAA9+8AAPgfAAD//wAA";
+
 #[middleware_fn]
 async fn plaintext(mut context: Ctx, _next: MiddlewareNext<Ctx>) -> MiddlewareResult<Ctx> {
-    let val = "Hello, World!";
-    context.body(val);
+    let val = r#"<html>
+<head>
+    <title>ghosty</title>
+    <link href="{{ICON}}" rel="icon" type="image/x-icon" />
+    <style>img { image-rendering: pixelated; width: 64px; height: 64px; }</style>
+</head>
+<body>
+    <img src="{{ICON}}" />
+    <div>todo</div>
+    <button disabled="true">copy</button>
+</body>
+</html>"#.replace("{{ICON}}", ICON);
+    context.body(&val);
     Ok(context)
 }
 
