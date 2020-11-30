e334fe7fa4737fe932acb5834fdded7a9ae069b0
John Teske <john.teske@protonmail.com>
2020-11-30 08:37:33 -0800

    Use match over if/else for routing

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,14 @@ use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
 
-//use std::sync::Arc;
-//use std::sync::RwLock;
-//use std::time::{Duration, Instant};
+// use std::sync::Arc;
+// use std::sync::RwLock;
+// use std::time::{Duration, Instant};
 
 #[cfg(not(feature = "serve-files"))]
 include!(concat!(env!("OUT_DIR"), "/html.rs"));
 
-//const TIMEOUT: Duration = Duration::from_secs(120);
+// const TIMEOUT: Duration = Duration::from_secs(120);
 
 fn main() {
     let listener = TcpListener::bind("0.0.0.0:4321").unwrap();
@@ -27,20 +27,20 @@ fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
     stream.read(&mut buffer).unwrap();
 
-    let get = b"GET / HTTP/1.1\r\n";
-
-    let (status_line, filename) = if buffer.starts_with(get) {
-        ("HTTP/1.1 200 OK\r\n\r\n", "src/index.html")
-    } else if buffer.starts_with(b"GET /value") {
-        ("HTTP/1.1 200 OK\r\n\r\n", "src/i")
-    } else {
-        ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
+    let (status_line, filename, message_body) = match buffer {
+        b if b.starts_with(b"GET / HTTP") => {
+            ("HTTP/1.1 200 OK\r\n\r\n", Some("src/index.html"), HTML)
+        }
+        b if b.starts_with(b"GET /value HTTP") => {
+            ("HTTP/1.1 200 OK\r\n\r\n", None, "{ \"value\": \"TODO\" }")
+        }
+        _ => ("HTTP/1.1 404 NOT FOUND\r\n\r\n", None, ""),
     };
 
     #[cfg(feature = "serve-files")]
     let contents = fs::read_to_string(filename).unwrap();
     #[cfg(not(feature = "serve-files"))]
-    let contents = filename;
+    let contents = message_body;
 
     let response = format!("{}{}", status_line, contents);
 
