1c5384627e0c58c19f4c077da35cf8c34cb29646
John Teske <john.teske@protonmail.com>
2020-11-30 13:14:34 -0800

    Add placeholder setter (POST /value)

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,8 @@ use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
 
-// use std::sync::Arc;
-// use std::sync::RwLock;
+use std::sync::Arc;
+use std::sync::RwLock;
 // use std::time::{Duration, Instant};
 
 #[cfg(not(feature = "serve-files"))]
@@ -16,24 +16,47 @@ include!(concat!(env!("OUT_DIR"), "/html.rs"));
 const OK_200: &str = "200 OK";
 const NOT_FOUND_404: &str = "404 NOT FOUND";
 
+struct State {
+    value: String,
+}
+
 fn main() {
     let listener = TcpListener::bind("0.0.0.0:4321").unwrap();
+
+    let state = Arc::new(RwLock::new(State {
+        value: "UNSET".to_string(),
+    }));
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        handle_connection(stream);
+        handle_connection(stream, state.clone());
     }
 }
 
-fn handle_connection(mut stream: TcpStream) {
+fn handle_connection(mut stream: TcpStream, state: Arc<RwLock<State>>) {
     let mut buffer = [0; 1024];
     stream.read(&mut buffer).unwrap();
 
-    let (status, filename, message_body) = match buffer {
-        b if b.starts_with(b"GET / HTTP") => (OK_200, Some("src/index.html"), HTML),
-        b if b.starts_with(b"GET /value HTTP") => (OK_200, None, "{ \"value\": \"TODO\" }"),
-        _ => (NOT_FOUND_404, None, ""),
+    let (status, filename, message_body): (&str, Option<&str>, String) = match buffer {
+        // assets
+        b if b.starts_with(b"GET / HTTP") => (OK_200, Some("src/index.html"), HTML.to_string()),
+        // values
+        b if b.starts_with(b"GET /value HTTP") => {
+            let st = state.clone();
+            let st = st.read().unwrap();
+            (OK_200, None, format!("{{ \"value\": \"{}\" }}", st.value))
+        }
+        b if b.starts_with(b"POST /value HTTP") => {
+            let st = state.clone();
+            let mut st = st.write().unwrap();
+            *st = State {
+                value: "TODO".to_string(),
+            };
+            (OK_200, None, format!("{{ \"value\": \"{}\" }}", st.value))
+        }
+        // 404
+        _ => (NOT_FOUND_404, None, "".to_string()),
     };
 
     #[cfg(feature = "serve-files")]
