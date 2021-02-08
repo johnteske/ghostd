a298652e85ae6e62d9261eae0c715504f3dd442e
John Teske <john.teske@protonmail.com>
2021-02-08 12:52:05 -0800

    Add in-memory state

--- examples/single-thread.rs
+++ examples/single-thread.rs
@@ -15,12 +15,19 @@
 //! documentation it looks like simple comparisons are supported, so only the max
 //! lifetime duration and timestamp are needed.
 //!
-//! 3. Error handling. I wanted to add a guard to the error checking to ensure we were
+//! 3. Error handling. I wanted to add a guard to the error checking to ensure I was
 //! only permitting expected errors. For the purposes of this example, any other error
 //! panics.
+//!
+//! 4. Parsing the request. The next step for me is to get and set state, for which I
+//! need to know the HTTP method. I added parse_start_line, not as a meaningful fn but
+//! mainly to abstract the parsing out of the main loop. In the real implementation it
+//! will be replaced by robust parsing.
+//!
+//! 5. State. With this single-threaded model, I don't believe a lock is required.
 
-use std::io::ErrorKind;
-use std::net::TcpListener;
+use std::io::{ErrorKind, Read};
+use std::net::{TcpListener, TcpStream};
 use std::thread;
 use std::time::{Duration, Instant};
 
@@ -31,13 +38,25 @@ fn main() {
     let listener = TcpListener::bind("127.0.0.1:4321").unwrap();
     listener.set_nonblocking(true).unwrap();
 
+    let mut state = String::new();
     let mut timestamp: Option<Instant> = None;
 
     loop {
         match listener.accept() {
-            Ok((_socket, addr)) => {
-                timestamp = Some(Instant::now());
-                println!("client: {:?}", addr)
+            Ok((stream, _addr)) => {
+                let start_line = parse_start_line(stream);
+                println!("{}", start_line);
+
+                match start_line.get(0..4).unwrap() {
+                    "GET " => {}
+                    "POST" => {
+                        timestamp = Some(Instant::now());
+                        state = "something".to_string()
+                    }
+                    _ => {}
+                }
+
+                println!("state:\t{}", state);
             }
             Err(ref e) if e.kind() == ErrorKind::WouldBlock => {}
             Err(e) => panic!("{}", e),
@@ -47,10 +66,19 @@ fn main() {
             println!("elapsed: {}", ts.elapsed().as_secs());
             if ts.elapsed() >= MAX_ELAPSED {
                 println!("reached MAX_ELAPSED");
+                state.clear();
                 timestamp = None;
             }
         }
 
         thread::sleep(TICK_RATE);
     }
+}
+
+fn parse_start_line(mut stream: TcpStream) -> String {
+    let mut buffer = [0; 32];
+    stream.read(&mut buffer).unwrap();
+    let request = String::from_utf8(buffer.to_vec()).unwrap();
+    let start_line = request.lines().next().unwrap();
+    start_line.to_string()
 }
