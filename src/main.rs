2fbf46d9eb5c5590b68144dd89374ddfdeb6f9e1
John Teske <john.teske@protonmail.com>
2021-03-03 19:10:35 -0800

    Adapt hyper single-threaded example

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,61 @@
-use std::net::TcpListener;
-use std::sync::{Arc, Mutex};
+// https://github.com/hyperium/hyper/blob/master/examples/single_threaded.rs
+// https://github.com/hyperium/hyper/blob/master/examples/web_api.rs
+#![deny(warnings)]
 
-mod handler;
-mod tmp_state;
+use hyper::service::{make_service_fn, service_fn};
+use hyper::{Body, Error, Method, Response, Server, StatusCode};
+
+static NOTFOUND: &[u8] = b"Not Found";
 
 fn main() {
-    let state = Arc::new(Mutex::new("".to_string()));
-    let tx = tmp_state::start(Arc::clone(&state));
+    // Configure a runtime that runs everything on the current thread
+    let rt = tokio::runtime::Builder::new_current_thread()
+        .enable_all()
+        .build()
+        .expect("build runtime");
 
-    let listener = TcpListener::bind("0.0.0.0:4321").unwrap();
-    for stream in listener.incoming() {
-        let stream = stream.unwrap();
-        handler::connection(stream, tx.clone(), state.clone());
+    // Combine it with a `LocalSet,  which means it can spawn !Send futures...
+    let local = tokio::task::LocalSet::new();
+    local.block_on(&rt, run());
+}
+
+async fn run() {
+    let addr = ([127, 0, 0, 1], 3000).into();
+
+    let make_service = make_service_fn(move |_| async move {
+        Ok::<_, Error>(service_fn(move |req| async move {
+            match (req.method(), req.uri().path()) {
+                (&Method::GET, "/value") => Ok::<_, Error>(Response::new(Body::from("get"))),
+                (&Method::POST, "/value") => Ok::<_, Error>(Response::new(Body::from("post"))),
+                _ => Ok(Response::builder()
+                    .status(StatusCode::NOT_FOUND)
+                    .body(NOTFOUND.into())
+                    .unwrap()),
+            }
+        }))
+    });
+
+    let server = Server::bind(&addr).executor(LocalExec).serve(make_service);
+
+    println!("Listening on http://{}", addr);
+
+    // The server would block on current thread to await !Send futures.
+    if let Err(e) = server.await {
+        eprintln!("server error: {}", e);
+    }
+}
+
+// Since the Server needs to spawn some background tasks, we needed
+// to configure an Executor that can spawn !Send futures...
+#[derive(Clone, Copy, Debug)]
+struct LocalExec;
+
+impl<F> hyper::rt::Executor<F> for LocalExec
+where
+    F: std::future::Future + 'static, // not requiring `Send`
+{
+    fn execute(&self, fut: F) {
+        // This will spawn into the currently running `LocalSet`.
+        tokio::task::spawn_local(fut);
     }
 }
