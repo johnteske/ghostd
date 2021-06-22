63408b2df32e7682bab71409a956adc71138a5a3
John Teske <john.teske@protonmail.com>
2021-06-22 08:14:33 -0700

    Try with warp

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,25 @@
 //#![deny(warnings)]
 
 use tokio::time::Duration;
+use warp::Filter;
 
 mod state;
 
-mod server;
-
 const TTL: Duration = Duration::from_secs(5);
-static NOTFOUND: &[u8] = b"Not Found";
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() {
     let (_, tx) = state::start(TTL);
 
-    let addr = ([127, 0, 0, 1], 3000).into();
+    let get = warp::path("value")
+        .and(warp::get())
+        .and_then(|| async move {
+            if true {
+                Ok(format!("asd"))
+            } else {
+                Err(warp::reject::not_found())
+            }
+        });
 
-    let server = server::run(addr, tx);
-
-    println!("Listening on http://{}", addr);
-
-    if let Err(e) = server.await {
-        eprintln!("server error: {}", e);
-    }
+    warp::serve(get).run(([127, 0, 0, 1], 3000)).await;
 }
