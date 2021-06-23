cd16d1802e63e0d8e27bb715ff423864fcfb6ae5
John Teske <john.teske@protonmail.com>
2021-06-23 08:40:58 -0700

    Clarify build variables and errors

--- build.rs
+++ build.rs
@@ -5,12 +5,12 @@ use std::{env, fs, str};
 use urlencoding::encode;
 
 fn main() {
+    let src_dir = Path::new("src").join("assets");
+
     let out_dir = env::var_os("OUT_DIR").unwrap();
     let dest_path = Path::new(&out_dir).join("html.rs");
 
-    let src_dir = Path::new("src").join("assets");
-
-    let html_content = get_file_content(&src_dir.join("index.html"));
+    let html_content = get_contents(&src_dir.join("index.html"));
 
     Command::new("npx")
         .arg("tsc")
@@ -18,11 +18,11 @@ fn main() {
         .arg("src/assets/script.ts")
         .spawn()
         .expect("typescript compiler failed");
-    let script_content = get_file_content(&src_dir.join("script.js"));
+    let script_content = get_contents(&src_dir.join("script.js"));
 
-    let style_content = get_file_content(&src_dir.join("style.css"));
+    let style_content = get_contents(&src_dir.join("style.css"));
 
-    let icon_content = get_file_content(&src_dir.join("favicon.svg"));
+    let icon_content = get_contents(&src_dir.join("favicon.svg"));
     let icon = format!("data:image/svg+xml,{}", encode(&icon_content));
 
     let template = r###"const HTML: &str = r##"@@HTML@@"##;"###
@@ -37,8 +37,8 @@ fn main() {
     println!("cargo:rerun-if-changed=src/assets/style.css");
 }
 
-fn get_file_content(path: &std::path::Path) -> String {
-    let file = fs::read(&path).expect("error reading file");
-    let content = str::from_utf8(&file).expect("error parsing file");
-    String::from(content)
+fn get_contents(path: &std::path::Path) -> String {
+    let bytes = fs::read(&path).expect("error reading file");
+    let contents = str::from_utf8(&bytes).expect("error converting bytes");
+    String::from(contents)
 }
